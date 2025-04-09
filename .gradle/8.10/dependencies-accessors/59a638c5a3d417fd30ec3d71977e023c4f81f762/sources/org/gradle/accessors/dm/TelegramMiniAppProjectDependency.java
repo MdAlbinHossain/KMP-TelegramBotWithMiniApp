f0c653e203c7b6941cbd9940ea77772880e7c42b
shadmanadman <adman.shadman@gmail.com>
2025-04-09 12:51:40 +0330

    Adding two other module to handel core and common codes

---
+++ .gradle/8.10/dependencies-accessors/59a638c5a3d417fd30ec3d71977e023c4f81f762/sources/org/gradle/accessors/dm/TelegramMiniAppProjectDependency.java
@@ -0,0 +1,35 @@
+package org.gradle.accessors.dm;
+
+import org.gradle.api.NonNullApi;
+import org.gradle.api.artifacts.ProjectDependency;
+import org.gradle.api.internal.artifacts.dependencies.ProjectDependencyInternal;
+import org.gradle.api.internal.artifacts.DefaultProjectDependencyFactory;
+import org.gradle.api.internal.artifacts.dsl.dependencies.ProjectFinder;
+import org.gradle.api.internal.catalog.DelegatingProjectDependency;
+import org.gradle.api.internal.catalog.TypeSafeProjectDependencyFactory;
+import javax.inject.Inject;
+
+@NonNullApi
+public class TelegramMiniAppProjectDependency extends DelegatingProjectDependency {
+
+    @Inject
+    public TelegramMiniAppProjectDependency(TypeSafeProjectDependencyFactory factory, ProjectDependencyInternal delegate) {
+        super(factory, delegate);
+    }
+
+    /**
+     * Creates a project dependency on the project at path ":client"
+     */
+    public ClientProjectDependency getClient() { return new ClientProjectDependency(getFactory(), create(":client")); }
+
+    /**
+     * Creates a project dependency on the project at path ":core"
+     */
+    public CoreProjectDependency getCore() { return new CoreProjectDependency(getFactory(), create(":core")); }
+
+    /**
+     * Creates a project dependency on the project at path ":server"
+     */
+    public ServerProjectDependency getServer() { return new ServerProjectDependency(getFactory(), create(":server")); }
+
+}
